2b61b8d5d123f12d4c3479b2e5044766581f61ac
unknown <operator@stacja42-01.szkola.zst-radom.edu.pl>
2025-03-11 14:15:55 +0100

    I've written some code, there are errors and I can't run the app, because Android Studio refuses to cooperate

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.clock;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextClock;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.Time;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
         EditText timeText = findViewById(R.id.editTextTime);
         TextView text = findViewById(R.id.textView);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IsIt1448((Time) timeText.getText());
+            }
+        });
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public boolean IsIt1448(Time alarm)
+    {
+        Instant currenTime;
+        Clock clock;
+        do{
+            currenTime = clock.instant();
+        }while(!currenTime.equals(alarm));
+        return true;
     }
 }
